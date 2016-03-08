7b8e78e303edee63b64076bb4f2c77dcdb71199f
Max Kordek <max@lisk.io>
2016-03-08 17:52:19 +0100

    Update

--- NEXT_STEPS.md
+++ NEXT_STEPS.md
@@ -1,3 +1,11 @@
 1. JS script which writes dummy data to the blockchain (started with forever) (only put)   
 2. UI API integration (only get)   
 3. UI if not logged in   
+
+
+
+tempIn = temperature
+
+powerCosts = power
+
+gasCosts = gas
--- dummyData/dummyData.py
+++ dummyData/dummyData.py
@@ -1,4 +1,8 @@
-payload = {'deviceId': 'liskchip', 'deviceName': 'Lisk C.H.I.P.', 'temperature': '20', 'power': '45', 'gas': '65'}
-r = requests.put("http://45.32.158.94:7000/api/dapps/524703051050655352/api/put/values", data=payload)
+#!/usr/bin/env python
+
+import requests
+
+payload = {'secret': '123','deviceId': 'liskchip', 'deviceName': 'Lisk C.H.I.P.', 'temperature': '20', 'power': '45', 'gas': '65'}
+r = requests.put("http://108.61.170.245:7000/api/dapps/17801572182567880835/api/put/values", data=payload)
 print r.status_code
 print r.content
