0c8e2316212ce630df88f3efb43eed8ebec79f7b
Max Kordek <max@lisk.io>
2016-03-10 12:03:26 +0100

    Finish v1
    With that PR the first version of the BlockData Dapp is finished!

--- dummyData/dummyData.py
+++ dummyData/dummyData.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python
 
 import requests
+import time
+import random
 
-payload = {'secret': '123','deviceId': 'liskchip', 'deviceName': 'Lisk C.H.I.P.', 'temperature': '20', 'power': '45', 'gas': '65', 'clock': '13-03-2016'}
-r = requests.put("http://108.61.170.245:7000/api/dapps/17801572182567880835/api/put/values", data=payload)
-print r.status_code
-print r.content
+temperature = 0
+power = 0
+gas = 0
+day = 0
+i = 0
+
+while True:
+	temperature = random.randint(16,24)
+	power = random.randint(95,105)
+	gas = random.randint(1,160)
+	i = i + 1 #Counter
+	day = str(i) #Convert int into string
+	if i <= 9:
+		day = '0'+day
+
+	payload = {'secret': '123','deviceId': '12475940823804898745L', 'deviceName': 'Lisk C.H.I.P.', 'temperature': temperature, 'power': power, 'gas': gas, 'clock': day+'-03-2016'}
+	r = requests.put("http://108.61.170.245:7000/api/dapps/17801572182567880835/api/put/values", data=payload)
+	print r.content
+	print payload
+
+	time.sleep(10)
+	if i == 30:
+		break
+print "END"
--- public/js/device.js
+++ public/js/device.js
@@ -1,58 +1,69 @@
-    var chart = new AmCharts.AmSerialChart(AmCharts.themes.dark);
-    chart.dataProvider = [{
-        day: 1,
-        value: -5
-    }, {
-        day: 2,
-        value: 3
-    }, {
-        day: 3,
-        value: 7
-    }, {
-        day: 4,
-        value: -3
-    }, {
-        day: 5,
-        value: 3
-    }, {
-        day: 6,
-        value: 4
-    }, {
-        day: 7,
-        value: 6
-    }, {
-        day: 8,
-        value: -3
-    }, {
-        day: 9,
-        value: -2
-    }, {
-        day: 10,
-        value: 6
-    }];
-    chart.categoryField = "day";
-    chart.autoMargins = false;
-    chart.marginLeft = 0;
-    chart.marginRight = 0;
-    chart.marginTop = 0;
-    chart.marginBottom = 0;
+    function getDevice() {
 
-    var graph = new AmCharts.AmGraph();
-    graph.valueField = "value";
-    graph.type = "column";
-    graph.fillAlphas = 1;
-    graph.showBalloon = false;
-    graph.lineColor = "#ffbf63";
-    graph.negativeFillColors = "#289eaf";
-    graph.negativeLineColor = "#289eaf";
-    chart.addGraph(graph);
+    jQuery.get(client + "api/dapps/" + dappId + "/api/get/values?deviceId=" + deviceId).done(function (data) {
 
-    var valueAxis = new AmCharts.ValueAxis();
-    valueAxis.gridAlpha = 0;
-    valueAxis.axisAlpha = 0;
-    chart.addValueAxis(valueAxis);
+        var deviceName = data.response.homeValues[data.response.homeValues.length-1].asset.deviceName;
+        document.getElementById("deviceName").innerHTML =  deviceName;
 
-    var categoryAxis = chart.categoryAxis;
-    categoryAxis.gridAlpha = 0;
-    categoryAxis.axisAlpha = 0;
-    chart.write("device");
+        var chart = new AmCharts.AmSerialChart(AmCharts.themes.dark);
+        chart.dataProvider = [{
+            day: 1,
+            value: -5
+        }, {
+            day: 2,
+            value: 3
+        }, {
+            day: 3,
+            value: 7
+        }, {
+            day: 4,
+            value: -3
+        }, {
+            day: 5,
+            value: 3
+        }, {
+            day: 6,
+            value: 4
+        }, {
+            day: 7,
+            value: 6
+        }, {
+            day: 8,
+            value: -3
+        }, {
+            day: 9,
+            value: -2
+        }, {
+            day: 10,
+            value: 6
+        }];
+        chart.categoryField = "day";
+        chart.autoMargins = false;
+        chart.marginLeft = 0;
+        chart.marginRight = 0;
+        chart.marginTop = 0;
+        chart.marginBottom = 0;
+
+        var graph = new AmCharts.AmGraph();
+        graph.valueField = "value";
+        graph.type = "column";
+        graph.fillAlphas = 1;
+        graph.showBalloon = false;
+        graph.lineColor = "#ffbf63";
+        graph.negativeFillColors = "#289eaf";
+        graph.negativeLineColor = "#289eaf";
+        chart.addGraph(graph);
+
+        var valueAxis = new AmCharts.ValueAxis();
+        valueAxis.gridAlpha = 0;
+        valueAxis.axisAlpha = 0;
+        chart.addValueAxis(valueAxis);
+
+        var categoryAxis = chart.categoryAxis;
+        categoryAxis.gridAlpha = 0;
+        categoryAxis.axisAlpha = 0;
+        chart.write("device");
+
+    });
+
+}
